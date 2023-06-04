4cb4a66c14a90e2c5396066c2007989ae6db8747
HeroGamers <hero@herogamers.dev>
2023-06-04 20:11:34 +0200

    Change design of footer and fix font errors in console

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,9 @@
 	import '../app.postcss';
 	import '../app.scss';
 	import Header from './Header.svelte';
-	// import "@fontsource/roboto";
-	// import "@fontsource-variable/open-sans";
+
+	import "@fontsource/roboto";
+	import "@fontsource-variable/open-sans";
 
 	const year = new Date().getFullYear();
 </script>
@@ -11,12 +12,12 @@
 <div class="flex flex-col md:flex-row min-h-screen">
 	<Header />
 
-	<div class="md:flex md:flex-col md:flex-1">
+	<div class="md:flex md:flex-col md:flex-1 relative">
 		<main class="flex flex-col flex-1 p-4 w-full h-full box-border">
 			<slot />
 		</main>
 
-		<footer class="flex flex-col justify-center items-center p-3 md:px-0 fixed bottom-0 left-0 w-full text-center pt-7">
+		<footer class="flex flex-col justify-center items-center p-3 md:px-0 sticky bottom-0 left-0 w-full text-center pt-7">
 			<div class="flex flex-row justify-center items-end">
 				<p>Marcus Sand &copy; {year} - CVR 43411764 - Hanstholmvej 55, 2. Th., 2720 Vanløse, Denmark</p>
 			</div>
--- src/routes/Portfolio.svelte
+++ src/routes/Portfolio.svelte
@@ -8,22 +8,32 @@
       project_name: "Portfolio Website",
       programming_language: "Svelte",
       link: "https://github.com/HeroGamers/Portfolio",
-      description: "This website!"
+      description: "This website, which is written in Svelte and replaces my old static portfolio website. Imagine " +
+        "having developed full-stack web applications and still having a static portfolio website. Not me, anymore!"
     },
     {
-      date: new Date("2019-08-10"),
-      project_name: "MyDog",
-      programming_language: "Java",
-      link: "https://github.com/HeroGamers/MyDog",
-      description: "A Minecraft plugin bringing much more functionality to tamed pets."
-    },
-    {
-      date: new Date("2023-06-03"),
+      date: new Date("2020-08-31"),
       project_name: "HeroAI",
       programming_language: "Python",
       link: "https://github.com/HeroGamers/HeroAI",
       description: "An AI written using TensorFlow Keras performing text classification using sentiment analysis on " +
         "text messages, to determine whether messages were toxic."
+    },
+    {
+      date: new Date("2021-03-15"),
+      project_name: "Anihaven Frontend",
+      programming_language: "Next.js",
+      link: "https://github.com/Anihaven/anihaven-web",
+      description: "A fully-fledged streaming website written in Next.js, made as a school project to try and " +
+        "challenge myself and explore new technologies."
+    },
+    {
+      date: new Date("2021-03-15"),
+      project_name: "Anihaven Backend",
+      programming_language: "GraphQL",
+      link: "https://github.com/Anihaven/anihaven-api",
+      description: "The backend for my fully-fledged streaming website, written in JavaScript with Koa and Apollo " +
+        "GraphQL, serving content to the Next.js frontend, with the content being hosted on S3 buckets."
     },
     {
       date: new Date("2020-08-12"),
@@ -33,7 +43,14 @@
       description: "A website based on a Danish school tradition during Christmas, where people can register and " +
         "get their very own Nisseven. It is essentially Secret Santa. The essential part of the project was the " +
         "security and encryption of others' Nissevenner / Secret Santa, and of course the login password."
-    }
+    },
+    {
+      date: new Date("2019-08-10"),
+      project_name: "MyDog",
+      programming_language: "Java",
+      link: "https://github.com/HeroGamers/MyDog",
+      description: "A Minecraft plugin bringing much more functionality to tamed pets."
+    },
   ]
 </script>
 
