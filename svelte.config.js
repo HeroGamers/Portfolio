9e97534ef7ff14a6e6a069b86cd6c0e7558bc85f
HeroGamers <hero@herogamers.dev>
2023-06-04 20:49:52 +0200

    Add Cloudflare pages adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,7 +7,13 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			// Default options from https://kit.svelte.dev/docs/adapter-cloudflare#usage
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			}
+		})
 	},
 
 	preprocess: [
