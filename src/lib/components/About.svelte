255eacbefb7fcac16f157e6999af4ec14de105af
HeroGamers <hero@herogamers.dev>
2024-11-17 19:43:05 +0100

    feat: Portfolio UX update, experiences update

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -5,22 +5,27 @@
   const age = Math.abs(ageDate.getUTCFullYear() - 1970);
 </script>
 
-<section id="about" class="flex flex-col pt-10 items-start w-full max-w-5xl">
-  <h5 class="font-medium">About me</h5>
-  <p>
-    Hi there, my name is Marcus, and I'm a {age} year old Software Engineering student at the Technical University of
-    Denmark with a love for all things tech. I currently reside in Copenhagen, Denmark.
-  </p>
-  <p class="pt-2">
-    I have always been fascinated by computers, and especially video games.
-    This passion slowly grew into deep love for how software, websites and other feats were created,
-    and as such I started creating simple websites and Minecraft Plugins, continuing to improve my skills and
-    expand upon my knowledge and expertise.
-  </p>
-  <p class="pt-2">
-    I have had the pleasure of working on lots of different projects, ranging from simple websites in plain HTML,
-    to full-stack web applications and AI models with TensorFlow and PyTorch.
-    This website is one of my little side-projects, and the primary objective is for it to function as a proper
-    portfolio.
-  </p>
+<section id="about" class="flex flex-col pt-10 w-full items-center">
+  <div class="flex flex-col items-start max-w-5xl">
+    <h5 class="font-medium text-center w-full">About me</h5>
+    <p>
+      Hi there, my name is Marcus, and I'm a {age}-year-old Security Analyst currently working full-time in Cybersecurity
+      while living in Copenhagen, Denmark.
+      
+      Starting next year, I'll be pursuing an <a href="https://www.en.aau.dk/education/master/cyber-security" rel="_blank">MSc in Cyber Security</a> to deepen my expertise and expand my career in this dynamic field.
+    </p>
+    <p class="pt-2">
+      My journey into tech began with a fascination for computers and video games,
+      which evolved into a passion for understanding how systems are built and secured.
+      
+      I started by creating websites and Minecraft plugins,
+      which later grew into developing full-stack applications and experimenting with AI models using TensorFlow and PyTorch.
+      This natural curiosity eventually led me to focus on cybersecurity, where I thrive on solving complex challenges and protecting systems from threats.
+    </p>
+    <p class="pt-2">
+      I've worked on a variety of projects, from enhancing security tools and workflows in professional settings
+      to participating in Capture the Flag (CTF) competitions with Kalmarunionen, which sharpen my practical skills and problem-solving abilities.
+      This website serves as a portfolio showcasing my journey and accomplishments in technology and cybersecurity.
+    </p>
+  </div>
 </section>
--- src/lib/components/Title.svelte
+++ src/lib/components/Title.svelte
@@ -1,4 +1,4 @@
 <section id="home" class="flex flex-col w-full">
-  <h1 class="text-center">An Aspiring Developer</h1>
-  <h4 class="text-center">Going for the MSc programme in Computer Science and Engineering</h4>
+  <h1 class="text-center">An Aspiring Cybersecurity Enthusiast</h1>
+  <h4 class="text-center">Soon-to-be pursuing an MSc in Cyber Security</h4>
 </section>
