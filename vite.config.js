baf9aa6c8f274498d44e9a770da1f722591b9379
HeroGamers <hero@herogamers.dev>
2024-11-17 18:09:34 +0100

    chore: Update to SvelteKit 2

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,14 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	// https://sass-lang.com/documentation/breaking-changes/legacy-js-api/
+	// https://vite.dev/config/shared-options#css-preprocessoroptions
+	css: {
+		preprocessorOptions: {
+		  scss: {
+			api: 'modern-compiler' // or "modern"
+		  }
+		}
 	}
 });
